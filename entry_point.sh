4acaf248bb998d995f6fa2b5497e9fea19678914
lmbsog0 <guillaume.sottas@liebherr.com>
2021-01-14 10:19:21 +0100

    move to source directory before env configuration

--- entry_point.sh
+++ entry_point.sh
@@ -2,5 +2,6 @@ git clone -b thud git://git.yoctoproject.org/poky poky
 git clone -b thud git://git.yoctoproject.org/meta-raspberrypi poky/meta-raspberrypi
 mkdir poky/build
 cp -R conf poky/build
+cd poky
 source oe-init-build-env build
 bitbake rpi-basic-image
