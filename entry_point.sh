c988bbbdc0224821f8622d8682030d59562bc410
lmbsog0 <guillaume.sottas@liebherr.com>
2021-01-14 15:43:42 +0100

    move meta-raspberrypi layer outside of poky directory

--- entry_point.sh
+++ entry_point.sh
@@ -1,5 +1,5 @@
 git clone -b thud git://git.yoctoproject.org/poky poky
-git clone -b thud git://git.yoctoproject.org/meta-raspberrypi poky/meta-raspberrypi
+git clone -b thud git://git.yoctoproject.org/meta-raspberrypi meta-raspberrypi
 mkdir build
 cp -R conf build
 source ./poky/oe-init-build-env ./build
